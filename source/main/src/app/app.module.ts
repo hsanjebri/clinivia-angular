3e8616c3806da845a9150776441813a9eeefd59c
shadha <shadha@gmail.com>
2024-05-06 18:20:13 +0100

    Create app.module.ts

---
+++ source/main/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from '../environments/environment';
+import { initializeApp } from 'firebase/app';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    AngularFireAuthModule,
+    AngularFireAuthModule,
+    AppComponent,
+    NgMultiSelectDropDownModule.forRoot()
+
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule {
+
+  constructor() {
+    // Initialize Firebase app here
+    initializeApp(environment.firebase); // Use your Firebase configuration from environment.ts
+  }
+}
